9a5660a0b58673dab61f3dd04eb158cebe9cdc48
Christian Hadiwinoto <chrhad@comp.nus.edu.sg>
2018-02-16 22:14:51 +0800

    Added Chinese normalization script, fixed segment script

--- scripts/segment.sh
+++ scripts/segment.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Run Chinese word segmenter based on CTB model and lowercase the input
+TOPDIR=`dirname $0`/..
+SEGMENTDIR=${TOPDIR}/tools/segmenter
 
-SEGMENTDIR=`dirname $0`/../tools/segmenter
-
-python2 ${SEGMENTDIR}/segment.py -m ${SEGMENTDIR}/ctbModel -l ${SEGMENTDIR}/lex.GB.txt \
-    | perl `dirname $0`/detokenizer.perl -l en
+sed -f `dirname $0`/normalize-zh.sed \
+    | python2 ${SEGMENTDIR}/segment.py -m ${SEGMENTDIR}/ctbModel -l ${SEGMENTDIR}/lex.GB.txt \
+    | perl `dirname $0`/lowercase.perl
